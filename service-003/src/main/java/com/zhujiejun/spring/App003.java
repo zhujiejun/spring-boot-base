4ff8782f1d19cf533e29615b7a9c82048723486f
zhujiejun <2383322468@qq.com>
2020-08-25 17:01:17 +0800

    modify the profile config for module004

--- service-003/src/main/java/com/zhujiejun/spring/App003.java
+++ service-003/src/main/java/com/zhujiejun/spring/App003.java
@@ -2,7 +2,9 @@ package com.zhujiejun.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class App003 {
     public static void main(String[] args) {
---
+++ service-003/src/main/java/com/zhujiejun/spring/conf/Conf003.java
@@ -0,0 +1,11 @@
+package com.zhujiejun.spring.conf;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Conf003 {
+    /*@Bean
+    public ApplicationInfoManager applicationInfoManager() {
+        return new ApplicationInfoManager(new MyDataCenterInstanceConfig(), (ApplicationInfoManager.OptionalArgs) null);
+    }*/
+}
