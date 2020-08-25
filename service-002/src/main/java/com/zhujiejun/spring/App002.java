e43f759f58e7fecd84ba5189e3a881f882157703
zhujiejun <2383322468@qq.com>
2020-08-25 13:47:59 +0800

    add annotation @SpringBootApplication

--- service-002/src/main/java/com/zhujiejun/spring/App002.java
+++ service-002/src/main/java/com/zhujiejun/spring/App002.java
@@ -1,7 +1,9 @@
 package com.zhujiejun.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App002 {
     public static void main(String[] args) {
         SpringApplication.run(App002.class, args);
