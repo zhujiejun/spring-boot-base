61f8c4b8276b752a36484f954299682f84d31ba2
zhujiejun <2383322468@qq.com>
2020-08-25 14:01:52 +0800

    add module service 003

---
+++ service-003/src/main/java/com/zhujiejun/spring/App003.java
@@ -0,0 +1,11 @@
+package com.zhujiejun.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App003 {
+    public static void main(String[] args) {
+        SpringApplication.run(App003.class, args);
+    }
+}
