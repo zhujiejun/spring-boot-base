ba182bd0780271ff1811eb359aa22714f58eb3ea
zhujiejun <2383322468@qq.com>
2020-08-25 13:41:36 +0800

    add module service 001 002

---
+++ service-001/src/main/java/com/zhujiejun/spring/App001.java
@@ -0,0 +1,9 @@
+package com.zhujiejun.spring;
+
+import org.springframework.boot.SpringApplication;
+
+public class App001 {
+    public static void main(String[] args) {
+        SpringApplication.run(App001.class, args);
+    }
+}
---
+++ service-002/src/main/java/com/zhujiejun/spring/App002.java
@@ -0,0 +1,9 @@
+package com.zhujiejun.spring;
+
+import org.springframework.boot.SpringApplication;
+
+public class App002 {
+    public static void main(String[] args) {
+        SpringApplication.run(App002.class, args);
+    }
+}
