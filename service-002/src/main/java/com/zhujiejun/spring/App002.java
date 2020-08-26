fd6746a517f4232f0b154428943f1b126617d0d9
zhujiejun <2383322468@qq.com>
2020-08-26 23:55:59 +0800

    config sit uat profile for module002

--- service-002/src/main/java/com/zhujiejun/spring/App002.java
+++ service-002/src/main/java/com/zhujiejun/spring/App002.java
@@ -3,10 +3,10 @@ package com.zhujiejun.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@PropertySources({})
 @SpringBootApplication
 public class App002 {
     public static void main(String[] args) {
+
         SpringApplication.run(App002.class, args);
     }
 }
