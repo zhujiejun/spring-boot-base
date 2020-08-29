3a693cc8770c270af30671d74c3827c4b3cd47a6
zhujiejun <2383322468@qq.com>
2020-08-29 19:41:02 +0800

    refactor some component config

--- spring-usage/src/main/java/com/zhujiejun/spring/bean/Order.java
+++ spring-usage/src/main/java/com/zhujiejun/spring/bean/Order.java
@@ -5,28 +5,28 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-//@Component
+@Component
 public class Order {
     public Order() {
-        System.out.println("第一步 执行无参数构造创建 bean 实例");
+        System.out.println("第一步 执行Order无参数构造创建 bean 实例");
     }
 
     private String oname;
 
     public void setOname(String oname) {
         this.oname = oname;
-        System.out.println("第二步 调用 set 方法设置属性值");
+        System.out.println("第二步 调用Order Set方法设置属性值");
     }
 
     //创建执行的初始化的方法
     @PostConstruct
     public void initMethod() {
-        System.out.println("第三步 执行初始化的方法");
+        System.out.println("第三步 执行Order初始化的方法");
     }
 
     //创建执行的销毁的方法
     @PreDestroy
     public void destroyMethod() {
-        System.out.println("第五步 执行销毁的方法");
+        System.out.println("第五步 执行Order销毁的方法");
     }
 }
--- spring-usage/src/main/java/com/zhujiejun/spring/proc/MyBeanPostProcessor.java
+++ spring-usage/src/main/java/com/zhujiejun/spring/proc/MyBeanPostProcessor.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Component;
 public class MyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("在初始化之前执行的方法,beanName=" + beanName + "beanType=" + bean.getClass());
+        System.out.println("在Bean初始化之前执行BeanPostProcessor的方法, beanName=" + beanName + ", beanType=" + bean.getClass());
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("在初始化之前执行的方法,beanName=" + beanName + "beanType=" + bean.getClass());
+        System.out.println("在Bean初始化之后执行BeanPostProcessor的方法, beanName=" + beanName + ", beanType=" + bean.getClass());
         return bean;
     }
 }
