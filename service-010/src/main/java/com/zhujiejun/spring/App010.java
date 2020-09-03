856773c6bb94bd8f0d1c4460c466239019f11bc6
zhujiejun <2383322468@qq.com>
2020-09-03 17:40:17 +0800

    add kafka module010

---
+++ service-010/src/main/java/com/zhujiejun/spring/App010.java
@@ -0,0 +1,11 @@
+package com.zhujiejun.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App010 {
+    public static void main(String[] args) {
+        SpringApplication.run(App010.class, args);
+    }
+}
