26504a13e8611fc38d8d727b900bffff38f65fbb
zhujiejun <2383322468@qq.com>
2020-09-29 01:33:26 +0800

    commit

---
+++ java-usage/src/main/java/com/zhujiejun/java/rxjava/AppNovel.java
@@ -0,0 +1,44 @@
+package com.zhujiejun.java.rxjava;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+import java.util.stream.IntStream;
+
+public class AppNovel {
+    public static void main(String[] args) {
+        //被观察者
+        Observable<String> novel = Observable.create(emitter -> {
+            IntStream.rangeClosed(1, 100).forEach(suffix -> {
+                emitter.onNext(String.format("content%d", suffix));
+            });
+            emitter.onComplete();
+        });
+        //观察者
+        Observer<String> reader = new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("1.onSubscribe");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println("2.the str is " + s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println("4.onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("3.onComplete");
+            }
+        };
+        novel.subscribe(reader);
+    }
+
+}
