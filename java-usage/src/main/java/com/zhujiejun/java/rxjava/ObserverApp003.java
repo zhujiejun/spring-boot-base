717cee9835363bab03c5092f0930fcd1660b58f8
zhujiejun <2383322468@qq.com>
2020-09-29 03:19:08 +0800

    commit

---
+++ java-usage/src/main/java/com/zhujiejun/java/rxjava/ObserverApp003.java
@@ -0,0 +1,41 @@
+package com.zhujiejun.java.rxjava;
+
+import com.google.common.base.Stopwatch;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.concurrent.TimeUnit;
+
+public class ObserverApp003 {
+
+    private static final String SAVE_PATH = "/home/cat/Downloads/ObserverApp003.tmp";
+
+    public static void main(String[] args) {
+        File file = new File(SAVE_PATH);
+        try {
+            if (!file.exists()) file.createNewFile();
+            System.setOut(new PrintStream(new FileOutputStream(file)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stopwatch watch = Stopwatch.createStarted();
+        Flowable.range(1, 100)
+                .observeOn(Schedulers.computation())
+                .map(v -> v * v)
+                .blockingSubscribe(System.out::println);
+        System.out.println("1.total time consumption is " + watch.elapsed(TimeUnit.MILLISECONDS) + " ms.");
+        System.out.println("----------------------------------");
+        watch.reset().start();
+        Flowable.range(1, 100)
+                .flatMap(v -> Flowable.just(v)
+                        .subscribeOn(Schedulers.computation())
+                        .map(w -> w * w)
+                ).blockingSubscribe(System.out::println);
+        System.out.println("2.total time consumption is " + watch.elapsed(TimeUnit.MILLISECONDS) + " ms.");
+        System.out.println("----------------------------------");
+    }
+}
