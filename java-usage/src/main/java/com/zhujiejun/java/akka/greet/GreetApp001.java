40c3c2dbb5181a3c40325d33f63cb381ed6d079d
zhujiejun <2383322468@qq.com>
2020-09-30 18:21:40 +0800

    commit

--- java-usage/src/main/java/com/zhujiejun/java/akka/greet/GreetApp001.java
+++ java-usage/src/main/java/com/zhujiejun/java/akka/greet/GreetApp001.java
@@ -11,10 +11,8 @@ import java.util.concurrent.TimeUnit;
 public class GreetApp001 {
     public static void main(String[] args) throws Exception {
         final ActorSystem actorSystem = ActorSystem.create("actorSystem");
-
         //创建一个到greeter Actor的管道
         final ActorRef greeter = actorSystem.actorOf(Props.create(Greeter.class), "greeter");
-
         //创建邮箱
         final Inbox inbox = Inbox.create(actorSystem);
 
@@ -22,7 +20,6 @@ public class GreetApp001 {
         greeter.tell(new Greetee("world"), ActorRef.noSender());
         //真正的发送消息,消息体为Greet
         inbox.send(greeter, new Greet());
-
         //等待5秒尝试接收Greeter返回的消息
         Greeting greeting1 = (Greeting) inbox.receive(Duration.create(5, TimeUnit.SECONDS));
         System.out.println("1.Greeting: " + greeting1.message);
@@ -31,11 +28,14 @@ public class GreetApp001 {
         greeter.tell(new Greetee("akka"), ActorRef.noSender());
         //发送第四个消息
         inbox.send(greeter, new Greet());
-
         //等待5秒尝试接收Greeter返回的消息
         Greeting greeting2 = (Greeting) inbox.receive(Duration.create(5, TimeUnit.SECONDS));
         System.out.println("2.Greeting: " + greeting2.message);
 
+        /**
+         * hello, akka
+         * hello, akka
+         */
         //新创建一个Actor的管道
         ActorRef greetPrinter = actorSystem.actorOf(Props.create(GreetPrinter.class));
         //使用schedule每一秒发送一个Greet消息给greeterActor,然后把greeterActor的消息返回给greetPrinterActor
