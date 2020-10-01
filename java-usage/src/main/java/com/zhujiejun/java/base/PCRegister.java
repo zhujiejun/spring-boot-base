e9e15e08fdeb835afa1870348b8608b9bfa7810b
zhujiejun <2383322468@qq.com>
2020-10-01 18:32:49 +0800

    commit

---
+++ java-usage/src/main/java/com/zhujiejun/java/base/PCRegister.java
@@ -0,0 +1,9 @@
+package com.zhujiejun.java.base;
+
+public class PCRegister {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+    }
+}
