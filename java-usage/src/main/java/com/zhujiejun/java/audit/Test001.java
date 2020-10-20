c536ca6771b08cc4e7acaf10e0a2e4b60d95bc6c
zhujiejun <2383322468@qq.com>
2020-10-20 14:08:14 +0800

    commit

--- java-usage/src/main/java/com/zhujiejun/java/audit/Test001.java
+++ java-usage/src/main/java/com/zhujiejun/java/audit/Test001.java
@@ -10,32 +10,18 @@ package com.zhujiejun.java.audit;
  * iload_1  //Load int from local variable onto the operand stack
  * iinc	    //Increment local variable by constant
  * ---------------------
- * int i = 1;
- * i = ++i;
+ * <p>int i = 1;
  * 0: iconst_1
  * 1: istore_1
- * <p>
- * 2: iinc          1, 1
- * 5: iload_1
- * <p>
- * 6: istore_1
- * 7: return
- * ---------------------
- * * int i = 1;
- * 0: iconst_1
- * 1: istore_1
- * <p>
- * * i = i++;
+ * <p>i = i++;
  * 2: iload_1
  * 3: iinc          1, 1
  * 6: istore_1
- * <p>
- * * int j = i++;
+ * <p>int j = i++;
  * 7: iload_1
  * 8: iinc          1, 1
  * 11: istore_2
- * <p>
- * * int k = i + ++i * i++;
+ * <p>int k = i + ++i * i++;
  * 12: iload_1
  * 13: iinc          1, 1
  * 16: iload_1
@@ -44,6 +30,40 @@ package com.zhujiejun.java.audit;
  * 21: imul
  * 22: iadd
  * 23: istore_3
+ * <p>System.out.println("i = " + i);//i=4
+ * 24: getstatic     #2                  // Field java/lang/System.out:Ljava/io/PrintStream;
+ * 27: new           #3                  // class java/lang/StringBuilder
+ * 30: dup
+ * 31: invokespecial #4                  // Method java/lang/StringBuilder."<init>":()V
+ * 34: ldc           #5                  // String i =
+ * 36: invokevirtual #6                  // Method java/lang/StringBuilder.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;
+ * 39: iload_1
+ * 40: invokevirtual #7                  // Method java/lang/StringBuilder.append:(I)Ljava/lang/StringBuilder;
+ * 43: invokevirtual #8                  // Method java/lang/StringBuilder.toString:()Ljava/lang/String;
+ * 46: invokevirtual #9                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+ * <p>System.out.println("j = " + j);//j=1
+ * 49: getstatic     #2                  // Field java/lang/System.out:Ljava/io/PrintStream;
+ * 52: new           #3                  // class java/lang/StringBuilder
+ * 55: dup
+ * 56: invokespecial #4                  // Method java/lang/StringBuilder."<init>":()V
+ * 59: ldc           #10                 // String j =
+ * 61: invokevirtual #6                  // Method java/lang/StringBuilder.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;
+ * 64: iload_2
+ * 65: invokevirtual #7                  // Method java/lang/StringBuilder.append:(I)Ljava/lang/StringBuilder;
+ * 68: invokevirtual #8                  // Method java/lang/StringBuilder.toString:()Ljava/lang/String;
+ * 71: invokevirtual #9                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+ * <p>System.out.println("k = " + k);//k=11
+ * 74: getstatic     #2                  // Field java/lang/System.out:Ljava/io/PrintStream;
+ * 77: new           #3                  // class java/lang/StringBuilder
+ * 80: dup
+ * 81: invokespecial #4                  // Method java/lang/StringBuilder."<init>":()V
+ * 84: ldc           #11                 // String k =
+ * 86: invokevirtual #6                  // Method java/lang/StringBuilder.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;
+ * 89: iload_3
+ * 90: invokevirtual #7                  // Method java/lang/StringBuilder.append:(I)Ljava/lang/StringBuilder;
+ * 93: invokevirtual #8                  // Method java/lang/StringBuilder.toString:()Ljava/lang/String;
+ * 96: invokevirtual #9                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+ * 99: return
  */
 public class Test001 {
     public static void main(String[] args) {
@@ -56,6 +76,6 @@ public class Test001 {
 
         System.out.println("i = " + i);//i=4
         System.out.println("j = " + j);//j=1
-        System.out.println("k = " + k);//k=11*/
+        System.out.println("k = " + k);//k=11
     }
 }
