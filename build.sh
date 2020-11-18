340ac2084558ee45c633dabff0412b7768ac958b
zhujiejun <2383322468@qq.com>
2020-11-18 19:34:10 +0800

    commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+version=1.0.0
+echo -n 'input the action(clean build bootJar bootBuildImage DistTar):'
+read act
+for i in 000 001 002 003 004 010 011
+do
+	./gradlew clean :service-$i:$act --imageName=zhujiejun.com/service-$i:$version -q -x test
+done
