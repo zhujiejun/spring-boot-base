66be436f6bcfc3b6e967bdb597eb21fb9f252fb5
zhujiejun <2383322468@qq.com>
2020-11-18 19:45:30 +0800

    commit

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ echo -n 'input the action(clean build bootJar bootBuildImage DistTar):'
 read act
 for i in 000 001 002 003 004 010 011
 do
-	./gradlew clean :service-$i:$act --imageName=zhujiejun.com/service-$i:$version -q -x test
+	if [[ $act == 'bootBuildImage' ]]; then
+		./gradlew clean :service-$i:$act --imageName=zhujiejun.com/service-$i:$version -q -x test;
+	else
+		./gradlew clean :service-$i:$act -q -x test;
+	fi
 done
