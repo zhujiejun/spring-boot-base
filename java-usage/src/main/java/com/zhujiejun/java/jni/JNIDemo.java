55861f08f9ae4f16ae22dfc4e09e2815f9d6b705
zhujiejun <2383322468@qq.com>
2020-11-25 20:20:11 +0800

    commit

--- java-usage/src/main/java/com/zhujiejun/java/jni/JNIDemo.java
+++ java-usage/src/main/java/com/zhujiejun/java/jni/JNIDemo.java
@@ -1,17 +1,32 @@
 package com.zhujiejun.java.jni;
 
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.TreeMap;
+
 public class JNIDemo {
     static {
+        //JVM System Args
+        //JVM Bootstrapt Args
         //-Djava.library.path=/opt/workspace/java/spring-boot-base/java-usage/lib
-        //String libPath = "/opt/workspace/java/spring-boot-base/java-usage/lib";
-        //System.out.println(System.getProperty("java.library.path"));
-        //System.setProperty("java.library.path", libPath);
         System.loadLibrary("jni-base");
     }
+
+    private final static String PATH = "/home/cat/Downloads/tmp/JNIDemo.tmp";
+
     public static native long hello(int max);
 
-    public static void main(String[] args) {
-        long hello = hello(1234567);
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get(PATH);
+        if (Files.notExists(path)) Files.createFile(path);
+        System.setOut(new PrintStream(path.toFile()));
+        Properties properties = System.getProperties();
+        new TreeMap<>(properties).forEach((k, v) -> System.out.println(k.toString().concat(": ").concat(v.toString())));
+        System.out.println("------------");
+        long hello = hello(12345678);
         System.out.println(hello);
     }
 }
