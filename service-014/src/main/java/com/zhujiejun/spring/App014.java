a220cadf713d332061a568010c4abe610ba4f8bf
zhujiejun <2383322468@qq.com>
2020-12-10 15:40:45 +0800

    commit

--- service-014/src/main/java/com/zhujiejun/spring/App014.java
+++ service-014/src/main/java/com/zhujiejun/spring/App014.java
@@ -1,14 +1,12 @@
 package com.zhujiejun.spring;
 
-import com.zhujiejun.spring.config.LoadBalancer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@LoadBalancerClient(name = "service-014", configuration = LoadBalancer.class)
+//@LoadBalancerClient(name = "service-014", configuration = Conf014.class)
 public class App014 {
     public static void main(String[] args) {
         SpringApplication.run(App014.class, args);
--- service-014/src/main/java/com/zhujiejun/spring/config/Conf014.java
+++ service-014/src/main/java/com/zhujiejun/spring/config/Conf014.java
@@ -1,9 +1,20 @@
 package com.zhujiejun.spring.config;
 
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.ServiceInstanceListSuppliers;
+import org.springframework.cloud.zookeeper.discovery.ZookeeperServiceInstanceListSupplier;
+import org.springframework.cloud.zookeeper.discovery.dependency.ZookeeperDependencies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class Conf014 {
@@ -11,5 +22,34 @@ public class Conf014 {
     @LoadBalanced
     WebClient.Builder lbWebClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    @Primary
+    public ServiceInstanceListSupplier defaultServiceInstanceListSupplier() {
+        //return new DefaultServiceInstanceListSuppler("service-013");
+        return new ZookeeperServiceInstanceListSupplier(ServiceInstanceListSuppliers.from("service-013"),
+                new ZookeeperDependencies());
+    }
+}
+
+class DefaultServiceInstanceListSuppler implements ServiceInstanceListSupplier {
+    private final String serviceId;
+
+    DefaultServiceInstanceListSuppler(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    @Override
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    @Override
+    public Flux<List<ServiceInstance>> get() {
+        return Flux.just(Arrays.asList(
+                new DefaultServiceInstance(serviceId + "1", serviceId, "localhost", 12013, false),
+                new DefaultServiceInstance(serviceId + "2", serviceId, "localhost", 22013, false)
+                /*new DefaultServiceInstance(serviceId + "3", serviceId, "localhost", 32013, false)*/));
     }
 }
