e938a72f866e569e0c1e231fc55a9a34eed3aaa9
zhujiejun <2383322468@qq.com>
2020-12-10 14:02:35 +0800

    commit

--- service-014/src/main/java/com/zhujiejun/spring/config/Conf014.java
+++ service-014/src/main/java/com/zhujiejun/spring/config/Conf014.java
@@ -1,7 +1,6 @@
 package com.zhujiejun.spring.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
