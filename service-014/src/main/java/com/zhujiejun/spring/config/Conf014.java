ab71df92d338a75ee21dbd9e3eeb1d6ac707cdde
zhujiejun <2383322468@qq.com>
2020-12-10 16:33:09 +0800

    commit

--- service-014/src/main/java/com/zhujiejun/spring/config/Conf014.java
+++ service-014/src/main/java/com/zhujiejun/spring/config/Conf014.java
@@ -1,34 +1,36 @@
 package com.zhujiejun.spring.config;
 
+import com.zhujiejun.spring.listen.Lsn014;
 import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
-import org.springframework.cloud.loadbalancer.support.ServiceInstanceListSuppliers;
-import org.springframework.cloud.zookeeper.discovery.ZookeeperServiceInstanceListSupplier;
-import org.springframework.cloud.zookeeper.discovery.dependency.ZookeeperDependencies;
+import org.springframework.cloud.zookeeper.discovery.watcher.DependencyWatcherListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 import java.util.List;
 
-//@Configuration
+@Configuration
 public class Conf014 {
-    @Bean
+    /*@Bean
     @LoadBalanced
     WebClient.Builder lbWebClientBuilder() {
         return WebClient.builder();
-    }
+    }*/
 
-    @Bean
+    /*@Bean
     @Primary
     public ServiceInstanceListSupplier defaultServiceInstanceListSupplier() {
         //return new DefaultServiceInstanceListSuppler("service-013");
         return new ZookeeperServiceInstanceListSupplier(ServiceInstanceListSuppliers.from("service-013"),
                 new ZookeeperDependencies());
+    }*/
+
+    @Bean
+    public DependencyWatcherListener defaultDependencyWatcherListener() {
+        return new Lsn014();
     }
 }
 
@@ -48,7 +50,7 @@ class DefaultServiceInstanceListSuppler implements ServiceInstanceListSupplier {
     public Flux<List<ServiceInstance>> get() {
         return Flux.just(Arrays.asList(
                 new DefaultServiceInstance(serviceId + "1", serviceId, "localhost", 12013, false),
-                new DefaultServiceInstance(serviceId + "2", serviceId, "localhost", 22013, false)
-                /*new DefaultServiceInstance(serviceId + "3", serviceId, "localhost", 32013, false)*/));
+                new DefaultServiceInstance(serviceId + "2", serviceId, "localhost", 22013, false),
+                new DefaultServiceInstance(serviceId + "3", serviceId, "localhost", 32013, false)));
     }
 }
---
+++ service-014/src/main/java/com/zhujiejun/spring/listen/Lsn014.java
@@ -0,0 +1,13 @@
+package com.zhujiejun.spring.listen;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.zookeeper.discovery.watcher.DependencyState;
+import org.springframework.cloud.zookeeper.discovery.watcher.DependencyWatcherListener;
+
+@Slf4j
+public class Lsn014 implements DependencyWatcherListener {
+    @Override
+    public void stateChanged(String dependencyName, DependencyState newState) {
+        log.info("----------the dependency name and dependency state are {}, {}----------", dependencyName, newState);
+    }
+}
