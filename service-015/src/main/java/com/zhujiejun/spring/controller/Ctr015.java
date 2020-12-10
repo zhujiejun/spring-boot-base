1c8b9311e0309c4db591275d71119778edfa92b4
zhujiejun <2383322468@qq.com>
2020-12-10 18:59:14 +0800

    commit

--- service-015/src/main/java/com/zhujiejun/spring/controller/Ctr015.java
+++ service-015/src/main/java/com/zhujiejun/spring/controller/Ctr015.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("service-015")
+@RequestMapping("/service-015")
 public class Ctr015 {
     @Value("${k1}")
     private String v1;
