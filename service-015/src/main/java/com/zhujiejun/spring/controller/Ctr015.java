3c7972fa473fa4ee9f03808bec271cc50dd414f7
zhujiejun <2383322468@qq.com>
2020-12-11 17:15:01 +0800

    commit

--- service-015/src/main/java/com/zhujiejun/spring/controller/Ctr015.java
+++ service-015/src/main/java/com/zhujiejun/spring/controller/Ctr015.java
@@ -1,34 +1,27 @@
 package com.zhujiejun.spring.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/service-015")
+@ConfigurationProperties(prefix = "spring.datasource")
 public class Ctr015 {
-    @Value("${k1}")
-    private String v1;
-    @Value("${k2}")
-    private String v2;
-    @Value("${k3}")
-    private String v3;
+    private String url;
+    private String username;
+    private String password;
+    private String driverClassName;
 
     @RequestMapping("/get/{key}")
     public String get(@PathVariable("key") String key) {
-        String v;
-        switch (key) {
-            case "k2":
-                v = v2;
-                break;
-            case "k3":
-                v = v3;
-                break;
-            default:
-                v = v1;
-                break;
-        }
-        return String.format("%s: %s\n", key, v);
+        return String.format("the data source info is: \n" +
+                        "url: %s\n" +
+                        "username: %s\n" +
+                        "password: %s\n" +
+                        "driverClassName: %s\n",
+                url, username, password, driverClassName);
     }
 }
